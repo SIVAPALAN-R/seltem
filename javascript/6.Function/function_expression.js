4b4db1d2500091e7aaa34d72cd8678fd24026d7a
SIVAPALAN R <132116789+SIVAPALAN-R@users.noreply.github.com>
2026-07-22 16:16:14 +0530

    Add files via upload

---
+++ javascript/6.Function/function_expression.js
@@ -0,0 +1,172 @@
+/* ======================================
+   Topic - Function Expression
+====================================== */
+
+/*
+Definition:
+A Function Expression
+is a function
+stored inside
+a variable.
+
+Simple Definition:
+A function expression
+is a function
+assigned to
+a variable.
+
+📖 Definition
+
+In a Function Expression,
+a function is created
+and stored
+inside a variable.
+
+The function
+can be called
+using the variable name.
+
+📝 Short Notes
+
+✔ Stored in a variable.
+
+✔ Called using
+   the variable name.
+
+✔ Can have
+   parameters.
+
+✔ Can return values.
+
+✔ Usually uses
+   let or const.
+
+✔ Function has
+   no name
+   (Anonymous Function).
+*/
+
+
+/*
+Exercise 1
+
+Create a variable
+named welcome.
+
+Store a function.
+
+Print
+
+"Welcome to JavaScript"
+
+Call the function.
+
+*/
+
+let welcomes = function () {
+
+    console.log("Welcome to JavaScript");
+
+};
+
+welcomes();
+
+
+/*
+Exercise 2
+
+Create a variable
+named hotel.
+
+Store a function.
+
+Print
+
+"Luxury Stay Hotel"
+
+Call the function.
+
+*/
+
+let hotels = function () {
+
+    console.log("Luxury Stay Hotel");
+
+}
+
+hotels();
+
+
+/*
+Challenge 1
+
+Create a variable
+named employee.
+
+Store a function.
+
+Print
+
+"Employee Login"
+
+Call the function.
+
+*/
+
+let employeed = function () {
+
+    console.log("Employee Login");
+
+};
+
+employeed();
+
+
+/*
+Challenge 2
+
+Create a variable
+named product.
+
+Store a function.
+
+Print
+
+"Laptop Added"
+
+Call the function.
+
+*/
+
+let products = function () {
+
+    console.log("Laptop Added");
+
+};
+
+products();
+
+
+/*
+Challenge 3
+
+Create a variable
+named course.
+
+Store a function.
+
+Print
+
+"JavaScript Completed"
+
+Call the function.
+
+*/
+
+let courses = function () {
+
+    console.log("JavaScript Completed");
+
+};
+
+courses();
